85fb41634257afee1352d1a0fe297782e1588f0a
kostytsAleksandr <kostyts.aleksandr@gmail.com>
2021-01-15 21:25:41 +0200

    EmptyProject

---
+++ CurrencyParserConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyParserConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
